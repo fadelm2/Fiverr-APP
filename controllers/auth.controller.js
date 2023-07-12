f0b695bce317a0f127ec9aa07d3b564a365b934c
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-07-13 06:26:38 +0700

    autcontroller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -13,7 +13,7 @@ export const register = async (req, res, next) => {
         await newUser.save();
         res.status(201).send("User has been created.");
     } catch (err) {
-        res.status(500).send("Something went wrong!");
+        res.status(500).send("Something went wcd .rong!");
     }
 }
 
--- server.js
+++ server.js
@@ -4,10 +4,12 @@ import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js"
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
+//import gigRoute from "./routes/gig.route.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
-mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true)
 
 const connect = async () => {
     try {
@@ -20,11 +22,14 @@ const connect = async () => {
 };
 
 
+
+app.use(cors({origin: 'http://localhost:5173', credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+//app.use("/api/gigs", gigRoute);
 
 app.listen(8800, ()=>{
     connect();
