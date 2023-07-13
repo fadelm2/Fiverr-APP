81ee3d452b01b4f100fabe4a4f104212e4e08b06
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-07-13 14:38:28 +0700

    update conversations

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoute from "./routes/auth.route.js";
 import gigRoute from "./routes/gig.route.js";
 import reviewRoute from "./routes/review.route.js";
 import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
 import cors from "cors";
 
 const app = express();
@@ -34,6 +35,8 @@ app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
 app.use("/api/reviews", reviewRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+
 
 app.listen(8800, ()=>{
     connect();
