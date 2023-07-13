7a7b189f4dbd9c6b8fbd9d65813d32106b896a64
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-07-13 18:26:41 +0700

    update order payment

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleware/jwt.js";
-import { getOrders,createOrders } from "../controllers/order.controller.js";
+import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
-router.post("/:gigId", verifyToken, createOrders);
+// router.post("/:gigId", verifyToken, createOrder);
 router.get("/", verifyToken, getOrders);
-//router.post("/create-payment-intent/:id", verifyToken, intent);
-//router.put("/", verifyToken, confirm);
+router.post("/create-payment-intent/:id", verifyToken, intent);
+router.put("/", verifyToken, confirm);
 
 export default router;
