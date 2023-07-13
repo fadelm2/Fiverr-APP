73eba9acefc7437cb596f01d035f95ba2d0cf63b
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-07-13 07:02:16 +0700

    update login register

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -6,7 +6,7 @@ const upload = async (file) => {
     data.append("upload_preset", "fiverr");
 
     try {
-        const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
+        const res = await axios.post("https://api.cloudinary.com/v1_1/dlbk80bmo/image/upload", data);
 
         const { url } = res.data;
         return url;
